ef59ad0bb3c3ad91048a52041ea04b17d1d572eb
Kiran S Bhushan <kiran.sukumaran@accenture.com>
2018-11-07 22:52:02 +1100

    added BeforeSuite

--- src/test/java/basicMaven/BaseTest.java
+++ src/test/java/basicMaven/BaseTest.java
@@ -1,10 +1,16 @@
 package basicMaven;
 
 import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class BaseTest {
+	
+	@BeforeSuite
+	public void testBeforeSuite() {
+		System.out.println("Run BEFORE every thing");
+	}
 	
 	@BeforeTest
 	public void testBefore( ) {

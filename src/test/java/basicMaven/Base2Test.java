2010173479c97393bd2f5befbedada7034d4a107
Kiran S Bhushan <kiran.sukumaran@accenture.com>
2018-11-09 10:22:59 +1100

    Testing Parameterization

--- src/test/java/basicMaven/Base2Test.java
+++ src/test/java/basicMaven/Base2Test.java
@@ -1,5 +1,6 @@
 package basicMaven;
 
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class Base2Test {
@@ -9,9 +10,11 @@ public class Base2Test {
 		System.out.println("testOne");
 	}
 	
+	@Parameters({"URL"})
 	@Test(groups= {"unitTest"})
-	public void testTwo() {
+	public void testTwo(String url) {
 		System.out.println("testTwo");
+		System.out.println("This is the URL for the Test Block 2" + url);
 	}
 
 }
--- src/test/java/basicMaven/BaseTest.java
+++ src/test/java/basicMaven/BaseTest.java
@@ -4,6 +4,7 @@ import org.testng.annotations.AfterSuite;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class BaseTest {
@@ -14,13 +15,15 @@ public class BaseTest {
 	}
 	
 	@BeforeTest
-	public void testBefore( ) {
+	public void testBefore() {
 		System.out.println("Run BEFORE Base test");
 	}
 	
+	@Parameters({"URL"})
 	@Test
-	public void testFive() {
+	public void testFive(String url) {
 		System.out.println("testFive");
+		System.out.println("THis is the URL for the test 1 block" + url);
 	}
 	
 	@Test(groups= {"unitTest"})
